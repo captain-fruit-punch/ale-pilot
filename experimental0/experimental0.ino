c931212ad84119d328249e20845705de718e3fe1
MrMai <ericmmai@gmail.com>
2017-02-15 14:08:29 -0800

    added motor and elevator stuff

--- experimental0/experimental0.ino
+++ experimental0/experimental0.ino
@@ -2,76 +2,97 @@
 // #include "TinyGPS++.h"
 
 // servos and turning
-Servo res; // servo right elevator
-int srep = 10; // servo right elevator pin
-double recp = 0; // right elevator current position
+Servo ras; // servo right aleron
+int srap = 10; // servo right elevator pin
 
-Servo les; // servo left elevator
-int slep = 9; // servo left elevator pin
-double lecp = 0;// left elevator current position
+Servo las; // servo left aleron
+int slap = 9; // servo left elevator pin
+
+Servo es; // servo elevator
+int sep = 11; // servo elevator pin
+
+Servo mt; // motor throtle
+int mp = 12; // motor pin
 
 int restpos = 90;
 
 void setup() {
-  res.attach(srep);
-  les.attach(slep);
-  res.write(90); // reset to position 0
-  les.write(90); // reset to position 0
+  // attah all servos
+  ras.attach(srap);
+  las.attach(slap);
+  se.attach(sep);
+  mt.attach(mp);
+  // reset everything
+  ras.write(restpos); // reset to rest
+  las.write(restpos); // reset to rest
+  se.write(restpos); // reset to rest
+  mt.write(0); // motor rest
+  // test
+  testsurfaces(500, 110, 70);
 }
 
 void loop() {
-  testsurfaces(500);
 }
 
-void testsurfaces(int delaytime) {
-  int max = 180;
-  int min = 0;
+void testsurfaces(int delaytime, int max, int min) {
   // test right
-  res.write(min);
+  ras.write(min);
   delay(delaytime);
-  res.write(max);
+  ras.write(max);
   delay(delaytime);
-  res.write(restpos);
+  ras.write(restpos);
   // test left
-  les.write(min);
+  las.write(min);
   delay(delaytime);
-  les.write(max);
+  las.write(max);
   delay(delaytime);
-  les.write(restpos);
+  las.write(restpos);
+  // test elevator
+  es.write(min);
+  delay(delaytime);
+  es.write(max);
+  delay(delaytime);
+  es.write(restpos);
   // test both
-  res.write(min);
-  les.write(min);
+  ras.write(min);
+  las.write(min);
+  es.write(min);
   delay(delaytime);
-  res.write(max);
-  les.write(max);
+  ras.write(max);
+  las.write(max);
+  es.write(max);
   delay(delaytime);
-  res.write(restpos);
-  les.write(restpos);
+  ras.write(restpos);
+  las.write(restpos);
+  es.write(restpos);
   // test inverse both
-  res.write(min);
-  les.write(max);
+  ras.write(min);
+  las.write(max);
+  es.write(max);
   delay(delaytime);
-  res.write(max);
-  les.write(min);
+  ras.write(max);
+  las.write(min);
+  es.write(min);
   delay(delaytime);
-  res.write(restpos);
-  les.write(restpos);
+  ras.write(restpos);
+  las.write(restpos);
+  es.write(restpos);
 }
 
-void quickrise(int incline, float risetime) { // incline from rest, time in seconds
+void quickrise(int incline, float risetime) { // incline from rast, time in seconds
   int intensity = incline + restpos;
   int settime = 500; // the time of the incline elevator in ms
-  res.write(intensity);
-  les.write(intensity);
+  ras.write(intensity);
+  las.write(intensity);
   delay(settime);
-  res.write(restpos);
-  les.write(restpos);
+  ras.write(restpos);
+  las.write(restpos);
   delay(risetime * 1000);
-  res.write(intensity - (2 * incline));
-  les.write(intensity - (2 * incline));
+  ras.write(intensity - (2 * incline));
+  las.write(intensity - (2 * incline));
   delay(settime);
-  res.write(restpos);
-  les.write(restpos);
+  ras.write(restpos);
+  las.write(restpos);
 }
 
 void quickglide()
