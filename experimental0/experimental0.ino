cd00e3117366c05089781bf71a3add7cb17ac36a
MrMai <ericmmai@gmail.com>
2017-02-16 08:11:31 -0800

    redid functions to new stuff

--- experimental0/experimental0.ino
+++ experimental0/experimental0.ino
@@ -3,18 +3,19 @@
 
 // servos and turning
 Servo ras; // servo right aleron
-int srap = 10; // servo right elevator pin
+int srap = 10; // servo right aleron pin
+int srarp = 90; // servo right aleron rest position
 
 Servo las; // servo left aleron
-int slap = 9; // servo left elevator pin
+int slap = 9; // servo left aleron pin
+int slarp = 90; // servo left aleron rest position
 
 Servo es; // servo elevator
 int sep = 11; // servo elevator pin
+int serp = 90; // servo elevator rest position
 
 Servo mt; // motor throtle
 int mp = 12; // motor pin
-
-int restpos = 90;
 
 void setup() {
   // attah all servos
@@ -23,9 +24,9 @@ void setup() {
   se.attach(sep);
   mt.attach(mp);
   // reset everything
-  ras.write(restpos); // reset to rest
-  las.write(restpos); // reset to rest
-  se.write(restpos); // reset to rest
+  ras.write(srarp); // reset to rest
+  las.write(slarp); // reset to rest
+  se.write(serp); // reset to rest
   mt.write(0); // motor rest
   // test
   testsurfaces(500, 110, 70);
@@ -40,19 +41,19 @@ void testsurfaces(int delaytime, int max, int min) {
   delay(delaytime);
   ras.write(max);
   delay(delaytime);
-  ras.write(restpos);
+  ras.write(srarp);
   // test left
   las.write(min);
   delay(delaytime);
   las.write(max);
   delay(delaytime);
-  las.write(restpos);
+  las.write(slarp);
   // test elevator
   es.write(min);
   delay(delaytime);
   es.write(max);
   delay(delaytime);
-  es.write(restpos);
+  es.write(serp);
   // test both
   ras.write(min);
   las.write(min);
@@ -62,9 +63,9 @@ void testsurfaces(int delaytime, int max, int min) {
   las.write(max);
   es.write(max);
   delay(delaytime);
-  ras.write(restpos);
-  las.write(restpos);
-  es.write(restpos);
+  ras.write(srarp);
+  las.write(slarp);
+  es.write(serp);
   // test inverse both
   ras.write(min);
   las.write(max);
@@ -74,25 +75,31 @@ void testsurfaces(int delaytime, int max, int min) {
   las.write(min);
   es.write(min);
   delay(delaytime);
-  ras.write(restpos);
-  las.write(restpos);
-  es.write(restpos);
+  ras.write(srarp);
+  las.write(slarp);
+  es.write(serp);
 }
 
-void quickrise(int incline, float risetime) { // incline from rast, time in seconds
-  int intensity = incline + restpos;
+void quickrise(float incline, float risetime, int speed, float intensity) { // incline from rast, time in seconds
   int settime = 500; // the time of the incline elevator in ms
-  ras.write(intensity);
-  las.write(intensity);
+  mt.write(speed);
+  ras.write(incline + srarp);
+  las.write(incline + slarp);
+  sep.write(intensity + serp);
   delay(settime);
-  ras.write(restpos);
-  las.write(restpos);
+  ras.write(srarp);
+  las.write(slarp);
+  es.write(serp);
   delay(risetime * 1000);
-  ras.write(intensity - (2 * incline));
-  las.write(intensity - (2 * incline));
+  ras.write(srarp - incline);
+  las.write(slarp - incline);
+  es.write(serp - intensity)
   delay(settime);
-  ras.write(restpos);
-  las.write(restpos);
+  ras.write(srarp);
+  las.write(slarp);
+  es.write(serp);
 }
 
-void quickglide()
+void quickglide() {
+
+}
